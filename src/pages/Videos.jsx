cd8fe60a896d1a4aee7abbc3ab48a7a0917dff77
Leo Espejo <leonardoespejo1978@gmail.com>
2026-04-07 22:23:50 +0200

    reducir archivos

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -16,7 +16,7 @@ export default function Videos() {
     { image: "/assets/Jornada_8.jpg",  text: "Jornada 8\nPALAUTORDERA-FCB",  url: "https://next.nubeespesan.duckdns.org/s/fk8b65f3BqYtFnN" },
     { image: "/assets/Jornada_9.jpg",  text: "Jornada 9\nFCB-ST. CUGAT",     url: "https://next.nubeespesan.duckdns.org/s/HkT8Ma8JxG4CMTM" },
     { image: "/assets/Jornada_10.jpg", text: "Jornada 10\nH. ST. QUIRZE-FCB", url: "https://next.nubeespesan.duckdns.org/s/cwd2gMcBnFNZFRg" },
-    { image: "/assets/amistos1.jpg", text: "Amistós\nFCB-DOMINICOS CAD", url: "https://next.nubeespesan.duckdns.org/s/kiJxjE33eomMeCW" },
+    { image: "/assets/amistos1.jpg", text: "Amistós\nFCB-DOMINICOS CAD", url: "https://next.nubeespesan.duckdns.org/s/igHmtxTJCz8R68o" },
     { image: "/assets/amistos2.jpg", text: "Amistós\nDOMINICOS-FCB", url: "https://next.nubeespesan.duckdns.org/s/dAFfY2rPNjqyzkN" },
     { image: "/assets/Jornada_11.jpg", text: "Jornada 11\nFCB-VIROLAI VORAMAR", url: "https://next.nubeespesan.duckdns.org/s/KeajsHmekstnd9r" },
     { image: "/assets/Jornada_12.jpg", text: "Jornada 12\nCREU ALTA-FCB", url: "https://next.nubeespesan.duckdns.org/s/N2KBENPym66qoJo" },
